a4cd7ab767d0e404d4e37555a846d515c870eb72
Jamil-Tero <eng_jamiltero@hotmail.com>
2019-01-23 11:23:43 +0400

    Updates

---
+++ precache-manifest.98c30731860ac31461eca2956f01f910.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a79ecdf415f244d6a490",
+    "url": "/jamil-tero-video-player/static/js/runtime~main.a79ecdf4.js"
+  },
+  {
+    "revision": "2f8ad1faec771eba7f4c",
+    "url": "/jamil-tero-video-player/static/js/main.2f8ad1fa.chunk.js"
+  },
+  {
+    "revision": "a162de45b9a017193f56",
+    "url": "/jamil-tero-video-player/static/js/1.a162de45.chunk.js"
+  },
+  {
+    "revision": "2f8ad1faec771eba7f4c",
+    "url": "/jamil-tero-video-player/static/css/main.4c07753d.chunk.css"
+  },
+  {
+    "revision": "d749f161b968cfda03746a1a4c209715",
+    "url": "/jamil-tero-video-player/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Jamil-Tero/jamil-tero-video-player/precache-manifest.56cceac32756b08e5675cbdc1ed63407.js"
+  "/jamil-tero-video-player/precache-manifest.98c30731860ac31461eca2956f01f910.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Jamil-Tero/jamil-tero-video-player/index.html", {
+workbox.routing.registerNavigationRoute("/jamil-tero-video-player/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
